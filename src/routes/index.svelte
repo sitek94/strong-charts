4c9f2c5a9d6b09b250bf784dbcc75e71a57cc27a
Maciek <msitkowski94@gmail.com>
2022-04-08 06:59:04 +0200

    use FileReader and d3.csvParse to parse uploaded CSV file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,24 @@
-<script context="module">
+<script lang="ts">
+  import * as d3 from 'd3'
+
+  let files
+
+  $: if (files) {
+    let file = files[0]
+
+    const reader = new FileReader()
+
+    reader.onabort = () => console.log('file reading was aborted')
+    reader.onerror = () => console.log('file reading has failed')
+    reader.onload = () => {
+      const rawText = reader.result.toString()
+      const csvText = rawText.replace(/;/g, ',')
+
+      const data = d3.csvParse(csvText)
+      console.log(data)
+    }
+    reader.readAsText(file)
+  }
 </script>
 
 <svelte:head>
@@ -7,6 +27,8 @@
 
 <div class="content">
   <h1>Import data</h1>
+
+  <input type="file" accept="text/csv" bind:files />
 </div>
 
 <style>

4528bf35822db00ef3cd68e3aa2db3e198d31211
Maciek <msitkowski94@gmail.com>
2022-04-10 12:48:02 +0200

    add `x` and `y` axis to ScatterChart

--- src/lib/charts/ScatterChart.svelte
+++ src/lib/charts/ScatterChart.svelte
@@ -3,13 +3,14 @@
   import { getInnerHeight, getInnerWidth } from './charts.utils'
   import MarginConvention from './MarginConvention.svelte'
   import type { Margin } from './charts.types'
+  import { format } from 'date-fns'
 
   // @ts-ignore
   type T = $$Generic
 
-  export let width = 500
+  export let width = 800
   export let height = 200
-  export let margin: Margin = { top: 20, right: 20, bottom: 20, left: 20 }
+  export let margin: Margin = { top: 20, right: 20, bottom: 20, left: 50 }
   export let data: T[] = []
   export let x: (d: T) => Date
   export let y: (d: T) => number
@@ -25,14 +26,42 @@
     .scaleTime()
     .domain(d3.extent(data, xValue) as [Date, Date])
     .range([0, innerWidth])
+    .nice()
+
   $: yScale = d3
     .scaleLinear()
     .domain([0, d3.max(data, yValue)] as [number, number])
-    .range([0, innerHeight])
+    .range([innerHeight, 0])
+    .nice()
+
+  $: xTicks = xScale.ticks(d3.timeWeek.every(1))
+  $: yTicks = yScale.ticks(10)
 </script>
 
 <svg {width} {height}>
   <MarginConvention {margin}>
+    <g>
+      {#each xTicks as tickValue}
+        <g class="tick" transform="translate({xScale(tickValue)},0)">
+          <line y2={innerHeight} />
+          <text style:text-anchor="middle" dy=".71em" y={innerHeight + 7}>
+            {format(tickValue, 'w')}
+          </text>
+        </g>
+      {/each}
+    </g>
+
+    <g>
+      {#each yTicks as tickValue}
+        <g class="tick" transform="translate(0,{yScale(tickValue)})">
+          <line x2={innerWidth} />
+          <text x={-10} dy=".32em" style:text-anchor="end">
+            {tickValue}
+          </text>
+        </g>
+      {/each}
+    </g>
+
     {#each data as d, i}
       <g>
         <title>{title(d)}</title>
@@ -48,3 +77,19 @@
     {/each}
   </MarginConvention>
 </svg>
+
+<style>
+  .tick {
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 0.725em;
+    font-weight: 200;
+  }
+  .tick line {
+    stroke: #333;
+    opacity: 0.2;
+  }
+  .tick text {
+    fill: #333;
+    text-anchor: start;
+  }
+</style>
--- src/routes/exercises/[exerciseId].svelte
+++ src/routes/exercises/[exerciseId].svelte
@@ -3,31 +3,41 @@
   import { page } from '$app/stores'
   import ScatterChart from '$lib/charts/ScatterChart.svelte'
 
+  const exerciseId = $page.params.exerciseId
+  let innerWidth = 700
+  let innerHeight = 350
+
   const exercise = getExercise($page.params.exerciseId)
-  const sets = getSets(exercise.id)
+  const sets = getSets(exerciseId)
 </script>
 
 <svelte:head>
-  <title>{exercise.name}</title>
+  <title>{exercise?.name}</title>
 </svelte:head>
 
-<div class="content">
-  <h1>
-    {exercise.name}
-  </h1>
+<svelte:window bind:innerHeight bind:innerWidth />
 
-  <ScatterChart
-    data={sets}
-    x={d => d['date']}
-    y={d => d['volume']}
-    title={d => `Volume: ${d.volume}`}
-  />
+<div class="content">
+  {#if exercise}
+    <h1>
+      {exercise.name}
+    </h1>
+
+    <ScatterChart
+      width={innerWidth}
+      data={sets}
+      x={d => d['date']}
+      y={d => d['volume']}
+      title={d => `Volume: ${d.volume}`}
+    />
+  {:else}
+    <h1>Loading...</h1>
+  {/if}
 </div>
 
 <style>
   .content {
     width: 100%;
-    max-width: var(--column-width);
     margin: var(--column-margin-top) auto 0 auto;
   }
 </style>

335325090271c645daff7918c4a622f42e912a5c
Maciek <msitkowski94@gmail.com>
2022-04-12 21:27:59 +0200

    create Alert component

---
+++ src/lib/Alert.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let status: 'success' | 'error'
+  export let message: string
+</script>
+
+<div role="alert" class={status}>
+  {message}
+</div>
+
+<style>
+  div {
+    padding: 1rem;
+    border: 1px solid var(--color);
+    border-radius: 0.25rem;
+    color: var(--color);
+    background-color: var(--bg-color);
+  }
+
+  .success {
+    --color: green;
+    --bg-color: #dff0d8;
+  }
+
+  .error {
+    --color: red;
+    --bg-color: #f2dede;
+  }
+</style>

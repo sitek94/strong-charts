d7887726844b1c57968fc3128aafaa2a49f5b2c2
Maciek <msitkowski94@gmail.com>
2022-04-15 06:22:18 +0200

    add example Circle component with tweened coords

---
+++ src/lib/charts/Circle.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { tweened } from 'svelte/motion'
+
+  export let cx
+  export let cy
+
+  const coords = tweened({ cx, cy }, { duration: 300 })
+
+  $: {
+    coords.set({ cx, cy })
+  }
+</script>
+
+<circle r={5} cx={$coords.cx} cy={$coords.cy} />
+
+<style>
+  circle {
+    fill: var(--accent-color);
+  }
+</style>
